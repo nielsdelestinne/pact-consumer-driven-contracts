70932af09bf1efd9c8ecfc7519bf459ce07440dd
nielsdelestinne <nielsdelestinne@gmail.com>
2020-04-22 12:16:08 +0200

    Provider fixes contract

--- pact-provider/src/main/java/be/niedel/pact/provider/domain/UserCreated.java
+++ pact-provider/src/main/java/be/niedel/pact/provider/domain/UserCreated.java
@@ -24,7 +24,7 @@ public class UserCreated implements DomainEvent {
     }
 
     public String toJson() {
-        return "{\"name\": \"" + nickname + "\",\"id\": \"" + id + "\"}";
+        return "{\"nickyname\": \"" + nickname + "\",\"id\": \"" + id + "\"}";
     }
 
 }
